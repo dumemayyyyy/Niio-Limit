3ac97f8a4f7d04c0dedd82e3822dd07cfdf2d9a0
dumemayyyyy <nhvctee@gmail.com>
2025-07-04 10:04:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,3 +34,10 @@ const startBot = () => {
 };
 
 startBot();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Bot is alive!"));
+app.listen(process.env.PORT || 3000, () => {
+  console.log("✅ Web server is running!");
+});
